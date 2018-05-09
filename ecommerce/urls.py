7305a58b534c0838fd10cbe1aff8f78242bf6abe
Aditya Singh <adityasingh222247@gmail.com>
2018-05-09 16:26:09 +0530

    commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import ProductListView
 
 urlpatterns = [
+	path('products/',ProductListView.as_view(),name='products'),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView
+
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
