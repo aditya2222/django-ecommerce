e592f627b642c9a447868f54232d5388ec92bbf6
Aditya Singh <adityasingh222247@gmail.com>
2018-05-09 22:12:21 +0530

    new commit

--- products/models.py
+++ products/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 import random
 import os
+
+
 # Create your models here.
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
-    name,ext = os.path.splitext(base_name)
+    name, ext = os.path.splitext(base_name)
     return name, ext
 
-def upload_image_path(instance,filename):
-    new_filename = random.randint(0,3910209312)
-    name,ext = get_filename_ext(filename)
-    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename,ext=ext)
-    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(0, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    image = models.ImageField(upload_to=upload_image_path,null=True,blank=True)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
