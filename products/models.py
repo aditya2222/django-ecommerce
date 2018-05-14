557a26907a7ef648d175ad69e72ff3f307e157a1
Aditya Singh <adityasingh222247@gmail.com>
2018-05-14 18:38:46 +0530

    filters commit

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import pre_save
 from .utils import unique_slug_generator
 from django.urls import reverse
 
+
 # Create your models here.
 
 def get_filename_ext(filepath):
@@ -22,25 +23,23 @@ def upload_image_path(instance, filename):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(default='abc',blank=True,unique=True)
+    slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def get_absolute_url(self):
-        return reverse("products:details",kwargs={'slug':self.slug})
+        return reverse("products:details", kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
 
 
-def product_pre_save_receiver(sender,instance,*args,**kwargs):
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
 
-pre_save.connect(product_pre_save_receiver,sender=Product)
 
-
-
-
+pre_save.connect(product_pre_save_receiver, sender=Product)
--- products/views.py
+++ products/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView, DetailView,TemplateView,CreateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView
 from .forms import UserCreationForm
 from django.urls import reverse_lazy
+
 
 # Create your views here.
 
 class HomePageView(TemplateView):
-	template_name = 'home_page.html'
+    template_name = 'home_page.html'
+
 
 class ProductListView(ListView):
 
     def get_queryset(self):
-        return Product.objects.all()    	
+        return Product.objects.all()
 
 
 class ProductDetailView(DetailView):
     model = Product
 
 
-
 class CreateUserView(CreateView):
-	form_class  = UserCreationForm
-	success_url = reverse_lazy('login')
-	template_name = 'registration/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
