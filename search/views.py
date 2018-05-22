2364756626ce1a2c3db148366536f24cf91ecf4e
Aditya <adityasingh222247@gmail.com>
2018-05-22 16:10:18 +0530

    new commit

--- search/views.py
+++ search/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from products.models import Product
+
+
 # Create your views here.
 
 
@@ -10,7 +12,7 @@ class SearchProductView(ListView):
     def get_queryset(self):
         # GET returns a key value pai and get lets us fetch  the value using the key returned by GET
         # shirt after the q acts as the alternative parameter in case the q does not exist or is none
-        query = self.request.GET.get('q',None)
+        query = self.request.GET.get('q', None)
         if query is not None:
             return Product.objects.filter(title__icontains=query)
         else:
