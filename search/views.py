05a442f762f3d7c46d55b6548643398c3bd6f064
Aditya <adityasingh222247@gmail.com>
2018-05-23 22:59:39 +0530

    Lookups

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from products.models import Product
+from django.db.models import Q
 
 
 # Create your views here.
@@ -12,9 +13,11 @@ class SearchProductView(ListView):
     def get_queryset(self):
         # GET returns a key value pai and get lets us fetch  the value using the key returned by GET
         # shirt after the q acts as the alternative parameter in case the q does not exist or is none
-        query = self.request.GET.get('q', None)
+        query = self.request.GET.get('q',None)
         if query is not None:
-            return Product.objects.filter(title__icontains=query)
+            lookups = Q(title__icontains=query) | Q(description__icontains=query)
+            return Product.objects.filter(lookups).distinct()
+
         else:
             return Product.objects.none()
 
