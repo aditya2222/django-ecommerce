63195929708e5ec81053f5d42bab9b7afc076fca
Aditya <adityasingh222247@gmail.com>
2018-05-25 22:13:03 +0530

    cart commit

--- carts/models.py
+++ carts/models.py
@@ -8,6 +8,24 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class CartManager(models.Manager):
+
+    def new_or_get(self, request):
+        cart_id = request.session.get('cart_id', None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            print("cart ID exists")
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            mew_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj
+
     def new(self, user=None):
         user_obj = None
         if user is not None:
