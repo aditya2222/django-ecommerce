e0b2b011b84c4a29e46166e80fd8403fd692ac58
Aditya <adityasingh222247@gmail.com>
2018-06-12 20:03:08 +0530

    Billing Profile Manager

--- billing/models.py
+++ billing/models.py
@@ -1,26 +1,47 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
-
+from accounts.models import GuestEmail
 # User contains current instance of the user
 User = get_user_model()
 
 # Create your models here.
 
+
+class BillingProfileManager(models.Manager):
+
+    def new_or_get(self, request):
+        user = request.user
+        guest_email_id = request.session.get('guest_email_id')
+        created = False
+        obj = None
+        if user.is_authenticated:
+            # logged in user checkout. It remembers payment stuff
+            obj, created = self.model.objects.get_or_create(
+                user=user, email=user.email)
+        elif guest_email_id is not None:
+            # guest user checkout. It reloads payment stuff
+            guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+            obj, created = self.model.objects.get_or_create(
+                email=guest_email_obj.email)
+        else:
+        	pass
+        return obj, created
+
+
 class BillingProfile(models.Model):
-	user = models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
-	email = models.EmailField()
-	active = models.BooleanField(default=True)
-	# auto_now updates time everytime object is saved
-	update = models.DateTimeField(auto_now=True)
-	# auto_now_add saves time once the object is created
-	timestamp = models.DateTimeField(auto_now_add=True)
-	# customer id in Stripe or Braintree
-
-
-	def __str__(self):
-		return self.email
-
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    # auto_now updates time everytime object is saved
+    update = models.DateTimeField(auto_now=True)
+    # auto_now_add saves time once the object is created
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # customer id in Stripe or Braintree
+    objects = BillingProfileManager()
+    def __str__(self):
+        return self.email
 
 
 # def billing_profile_created_receiver(sender, instance, created, *args, **kwrags):
@@ -31,8 +52,9 @@ class BillingProfile(models.Model):
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
-	if created and instance.email:
-		BillingProfile.objects.get_or_create(user=instance,	email=instance.email)
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(
+            user=instance,	email=instance.email)
 
 
 post_save.connect(user_created_receiver, sender=User)
