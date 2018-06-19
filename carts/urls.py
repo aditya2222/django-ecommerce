f076b18724669d66ac2423e951b3d116c47d1843
adityasingh222247@gmail.com <adityasingh222247@gmail.com>
2018-06-19 14:42:44 +0530

    Checkout Success

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,11 @@
 app_name = 'cart'
 from django.urls import path, include
-from .views import cart_home,cart_update,checkout_home
+from .views import cart_home, cart_update, checkout_home, checkout_done_view
+
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('checkout/success/', checkout_done_view, name='success'),
     path('update/', cart_update, name='update'),
     path('checkout/', checkout_home, name='checkout'),
+
 ]
