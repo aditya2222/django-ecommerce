347597f7542b0e5b03f69714944a405e50cc75af
Aditya Singh <adityasingh222247@gmail.com>
2018-08-03 11:48:06 +0530

    create the abstract base user

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+
+)
+
 
 # Create your models here.
+
+# Our Custom User Model
+class User(AbstractBaseUser):
+    email = models.EmailField()
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)  # can login
+    staff = models.BooleanField(default=False)  # staff user but not superuser
+    admin = models.BooleanField(default=False)  # superuser
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    # email and password are required by default
+    REQUIRED_FIELDS = []  # fields specified here come up at python3 manage.py createsuperuser
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class GuestEmail(models.Model):
     email = models.EmailField()
     active = models.BooleanField(default=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.email
