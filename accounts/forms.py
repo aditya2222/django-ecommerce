93ce5c2746994ac5b1b0d182e23bdddeb1e72d52
Aditya Singh <adityasingh222247@gmail.com>
2018-08-05 15:22:53 +0530

    update login and register forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -41,7 +41,7 @@ class UserAdminChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('full_name','email', 'password', 'active', 'admin')
+        fields = ('full_name', 'email', 'password', 'active', 'admin')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -50,17 +50,38 @@ class UserAdminChangeForm(forms.ModelForm):
         return self.initial["password"]
 
 
+class RegisterForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=120)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    
+    class Meta:
+        model = User
+        fields = ('full_name', 'email',)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.active = False  # send confirmation email
+        if commit:
+            user.save()
+        return user
 
 
 class GuestForm(forms.Form):
     email = forms.EmailField()
 
+
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=120)
+    username = forms.EmailField(max_length=120, label='Email')
     password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,20 @@
-from django.contrib.auth import login, authenticate,get_user_model
+from django.contrib.auth import login, authenticate, get_user_model
 from django.shortcuts import render, redirect
-from .forms import GuestForm,RegisterForm,LoginForm 
+from .forms import GuestForm, RegisterForm, LoginForm
 from django.utils.http import is_safe_url
 from .models import GuestEmail
 
 # Instance of the current user
 User = get_user_model()
 
+
 # Create your views here.
 
 # The form below is applicable for login and register and other auth forms  also
 
 def login_view(request):
-    form =  LoginForm(request.POST or None)
-    context = {"form":form}
+    form = LoginForm(request.POST or None)
+    context = {"form": form}
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
@@ -35,14 +36,12 @@ def login_view(request):
                 return redirect("/")
         else:
             print("Error")
-    return render(request,"accounts/login.html",context)
-
-
+    return render(request, "accounts/login.html", context)
 
 
 def guest_register_view(request):
-    form =  GuestForm(request.POST or None)
-    context = {"form":form}
+    form = GuestForm(request.POST or None)
+    context = {"form": form}
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
     redirect_path = next_ or next_post or None
@@ -50,12 +49,12 @@ def guest_register_view(request):
 
         email = form.cleaned_data.get("email")
         new_guest_email = GuestEmail.objects.create(email=email)
-        request.session['guest_email_id']=new_guest_email.id
+        request.session['guest_email_id'] = new_guest_email.id
         if is_safe_url(redirect_path, request.get_host()):
             return redirect(redirect_path)
         else:
             return redirect("accounts:register")
-        
+
     return redirect("accounts:register")
 
 
@@ -63,12 +62,8 @@ def guest_register_view(request):
 
 def register_view(request):
     form = RegisterForm(request.POST or None)
-    context = {"form":form}
+    context = {"form": form}
 
     if form.is_valid():
-        username = form.cleaned_data.get("username")
-        email = form.cleaned_data.get("email")
-        password = form.cleaned_data.get("password")
-        new_user = User.objects.create_user(username, email, password)
-    return render(request,"accounts/register.html",context)
-
+        form.save()
+    return render(request, "accounts/register.html", context)
