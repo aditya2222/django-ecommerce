57c33a384346c4be905efde804541c91a789d750
Aditya Singh <adityasingh222247@gmail.com>
2018-09-19 21:36:11 +0530

    Object Viewed model

--- analytics/models.py
+++ analytics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
@@ -10,6 +11,16 @@ User = settings.AUTH_USER_MODEL
 class ObjectViewed(models.Model):
     user = models.ForeignKey(
         User, blank=True, null=True, on_delete=models.CASCADE)
-    # ip_address
+    ip_address = models.CharField(max_length=220, blank=True, null=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s viewed on %s" % (self.content_object, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']  # most recent show up first
+        verbose_name = 'Object Viewed'
+        verbose_name_plural = 'Objects Viewed'
